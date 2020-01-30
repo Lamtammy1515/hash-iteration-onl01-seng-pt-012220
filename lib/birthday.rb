93c2f34cd6b1b848cc4d9e35ddd779d50bb89b58
Tammy Lam <lamtammy1515@gmail.com>
2020-01-30 19:22:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,5 @@ end
 
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids < 12 do |name, age|
-    print "Happy Birthday #{name}! You are now #{age} years old!"
-  end
+  birthday_kids < 12 {|name, age| print "Happy Birthday #{name}! You are now #{age} years old!"}
 end
